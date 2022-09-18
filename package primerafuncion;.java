5cda889db3bec8fdfdacd4a1da783b0e415af142
sebasescudero22 <74509186+sebasescudero22@users.noreply.github.com>
2022-09-17 22:37:01 -0500

    Add files via upload

---
+++ package primerafuncion;.java
@@ -0,0 +1,15 @@
+package primerafuncion;
+
+public class carro{
+
+    public static void  main (String[] args) {
+
+    }
+
+    public static int  suma ( int a, int b){
+
+         int c = a+b 
+         return c;
+    }
+    
+}
